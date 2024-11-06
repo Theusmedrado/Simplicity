a6df0cb74c41a8814ad3fddd84748b759d23a1a5
Theusmedrado <theusmedrado10@gmail.com>
2024-11-06 17:16:32 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -10,14 +10,64 @@ const botaoBuscar = document.querySelector("#buscar");
 const mensagemStatus = document.querySelector("#status");
 
 //Ouvinte de evento para o botão buscar
-botaoBuscar.addEventListener('click', function(){
+botaoBuscar.addEventListener('click', async function () {
     //verificar se o cep digitado não tem 9 dígitos
-    if(campoCep.value.length !== 9){
+    if (campoCep.value.length !== 9) {
         mensagemStatus.innerHTML = 'Digite um cep válido!';
         mensagemStatus.style.color = 'purple';
+
+        // para completamente a execução do código
         return;
-
-
-
     }
-})
+    // guardar o valor do cep digitado
+    let cepDigitado = campoCep.value;
+    console.log(cepDigitado);
+    /*
+    AJAX- Asyncronous JavaScript And XML
+    técnica de comunicação assíncrona (transmissão, recebimento)
+    de dados MUITO USADA entre diferentes tipos de sistemas (site, app, jogo, software) e tecnologias (php, java, asp, etc).
+    */
+
+    // etapa 1: preparamos o endereço junto com o cep digitado
+    let endereco = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+
+    // etapa 2: acesamos o viaCEP com o endereço ajustado 
+    const resposta = await fetch(endereco);
+
+    //etapa 3: extrair os dados que o via CEP processou
+    const dados = await resposta.json();// formato de OBJETO
+    console.log(dados);
+
+    //etapa 4: lidando com os dados (em caso de erro ou sucesso)
+    if ("erro" in dados) {
+        mensagemStatus.innerHTML = 'CEP inexistente!🙁';
+        mensagemStatus.style.color = 'red';
+    } else {
+        mensagemStatus.innerHTML = 'CEP encontrado!😎';
+        mensagemStatus.style.color = 'blue';
+
+
+        //selecionando os campos que estão escondidos
+        const campos = document.querySelectorAll(".campos-restantes");
+
+        // loop for para acessar cada campo e remover a classe
+        for (let i = 0; i < campos.length; i++) {
+            campos[i].classList.remove('campos-restantes');
+
+        campoEndereco.value = dados.logradouro;
+        campoBairro.value = dados.bairro;
+        campoCidade.value = dados.localidade;
+        campoEstado.value = dados.uf;
+        }
+    }
+});
+// final da função do botão busca
+
+
+
+
+
+
+
+
+
